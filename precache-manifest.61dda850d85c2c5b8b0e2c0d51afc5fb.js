d256d5096ccc475a46d1301c8c9d1d04f59efac4
a14rathore <87847257+a14rathore@users.noreply.github.com>
2021-11-09 18:43:07 +0530

    Updates

---
+++ precache-manifest.61dda850d85c2c5b8b0e2c0d51afc5fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb26516dca4ad7e225293d3c7987e698",
+    "url": "/NewsApp/index.html"
+  },
+  {
+    "revision": "3ac366702f62653cbe47",
+    "url": "/NewsApp/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "e4300987ce1ba9e8c2c2",
+    "url": "/NewsApp/static/js/2.31afa2c6.chunk.js"
+  },
+  {
+    "revision": "8d0df6a9f6b2d88ac068d142fd4b4030",
+    "url": "/NewsApp/static/js/2.31afa2c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20673c6705128b22227a",
+    "url": "/NewsApp/static/js/3.f41fec7e.chunk.js"
+  },
+  {
+    "revision": "3ac366702f62653cbe47",
+    "url": "/NewsApp/static/js/main.585963c6.chunk.js"
+  },
+  {
+    "revision": "54258e0197683c20f94e",
+    "url": "/NewsApp/static/js/runtime-main.78789224.js"
+  }
+]);
